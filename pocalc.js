223464e763c48a2bc9538c0bb2d7884429d63098
Miguel Marin <ElCateto300@gmail.com>
2019-01-03 10:06:11 -0500

    i done dome

---
+++ pocalc.js
@@ -0,0 +1,47 @@
+function one(){
+  document.getElementById("output").innerHTML ="1";
+}
+
+function two(){
+  document.getElementById("output").innerHTML ="2";
+}
+
+function tree(){
+  document.getElementById("output").innerHTML ="3";
+}
+
+function four(){
+  document.getElementById("output").innerHTML ="4";
+}
+
+function five(){
+  document.getElementById("output").innerHTML ="5";
+}
+
+function six(){
+  document.getElementById("output").innerHTML ="6";
+}
+
+function seven(){
+  document.getElementById("output").innerHTML ="7";
+}
+
+function eight(){
+  document.getElementById("output").innerHTML ="8";
+}
+
+function nine(){
+  document.getElementById("output").innerHTML ="9";
+}
+
+function zero(){
+  document.getElementById("output").innerHTML ="0";
+}
+
+function dot(){
+  document.getElementById("output").innerHTML =".";
+}
+
+function clear(){
+  ctx.clearRect(0, 0, output.width, output.height);
+}
