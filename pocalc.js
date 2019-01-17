74cb75fa8a3c49e7835bc6d912fb78150d14c51b
Miguel Marin <ElCateto300@gmail.com>
2019-01-17 10:49:38 -0500

    i done dome

--- pocalc.js
+++ pocalc.js
@@ -1,103 +1,24 @@
-let display
-let x
-let y
-let operations
-let memorie
-
-
-
-function one(){
-display = "1" + display
-  document.getElementById("displayScreen").value ="1";
+var numberOfNumbers = 0
+function num(number) {
+  if (numberOfNumbers < 9){
+    document.getElementById('displayScreen').value = document.getElementById('displayScreen').value + number;
+    numberOfNumbers++
+  }
 }
-
-function two(){
-  display = "2" + display
-  document.getElementById("displayScreen").value ="2";
+function oper(symbol){
+  document.getElementById('displayScreen').value = document.getElementById('displayScreen').value + symbol;
+  numberOfNumbers = 0;
 }
-
-function tree(){
-  display = "3" + display
-  document.getElementById("displayScreen").value ="3";
-}
-
-function four(){
-  display = "4" + display
-  document.getElementById("displayScreen").value ="4";
-}
-
-function five(){
-  display = "5" + display
-  document.getElementById("displayScreen").value ="5";
-}
-
-function six(){
-  display = "6" + display
-  document.getElementById("displayScreen").value ="6";
-}
-
-function seven(){
-  display = "7" + display
-  document.getElementById("displayScreen").value ="7";
-}
-
-function eight(){
-  display = "8" + display
-  document.getElementById("displayScreen").value ="8";
-}
-
-function nine(){
-  display = "9" + display
-  document.getElementById("displayScreen").value ="9";
-}
-
-function zero(){
-  display = "0" + display
-  document.getElementById("displayScreen").value ="0";
-}
-
-function dot(){
-  display = "." + display
-  document.getElementById("displayScreen").value =".";
-}
-
-function clear(){
-  carryNum = "";
-    carryOver = false;
-    displayCleared = false;
-    num1 = 0;
-    num2 = 0;
-    runningTotal = 0;
-    screenTotal = "";
-    display.value = "0";
-}
-function percent(){
-
-}
-
-function plusOrminus(){
-  display = "-" + display
-  document.getElementById("displayScreen").value ="-";
-}
-
-function divide(){
-  display = "/" + display
-  document.getElementById("displayScreen").value ="/";
-}
-
-function minus(){
-  display = "-" + display
-  document.getElementById("displayScreen").value ="-";
-}
-
-function plus(){
-  display = "+" + display
-  document.getElementById("displayScreen").value ="+";
-}
-
 function equals(){
+  let answer = eval(document.getElementById('displayScreen').value)
+  document.getElementById('displayScreen').value = answer
+}
+
+function claer(){
+    document.getElementById('displayScreen').value = 0
+    numberOfNumbers = 0;
 }
 
 function fun() {
-
+  var audio = document.getElementById("audio");
 }
